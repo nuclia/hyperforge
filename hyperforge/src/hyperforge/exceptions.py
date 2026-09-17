90fca371ba54b198d4c858b3eaf36187901abd0b
Ramon Navarro Bosch <ramon.nb@gmail.com>
2026-09-17 09:01:24 -0400

    OpenAI Chat support

--- hyperforge/src/hyperforge/exceptions.py
+++ hyperforge/src/hyperforge/exceptions.py
@@ -12,3 +12,11 @@ class MaxRetries(Exception):
 
 class ModelRetry(Exception):
     pass
+
+
+class OpenAIChatCompletionsError(Exception):
+    pass
+
+
+class NotSupportedbyLLMException(Exception):
+    pass
